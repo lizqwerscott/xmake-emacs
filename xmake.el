995f1ff5d4914f9a0c4e7be81ef65b801370ef95
lizqwerscott <lizqwerscott@163.com>
2024-06-22 18:43:51 +0800

    feat: Added the description of the package

--- xmake.el
+++ xmake.el
@@ -1,3 +1,34 @@
+;;; xmake.el --- Interface for xmake                 -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  lizqwer scott
+
+;; Author: lizqwer scott <lizqwerscott@gmail.com>
+;; Keywords: lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+(provide 'xmake)
+;;; xmake.el ends here
+
+
 (defun xmake-installed-p ()
   "Check if xmake is installed."
   (eq (call-process-shell-command "xmake --version") 0))
@@ -73,3 +104,4 @@
     (message "No xmake run process running.")))
 
 (provide 'xmake)
+;;; xmake.el ends here
